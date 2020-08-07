e44fb29e30d4977dfc3931596a5620d6783b0f04
Matheus Nathan <mnathan22@hotmail.com>
2020-08-07 14:35:23 -0300

    Projeto criado

---
+++ src/EntradaDados.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaDados {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		String x;
+		int y;
+		double z;
+
+		x = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
+
+		System.out.println("Você digitou: " + x);
+		System.out.println("Você digitou: " + y);
+		System.out.println("Você digitou: " + z);
+		sc.close();
+
+	}
+
+}
